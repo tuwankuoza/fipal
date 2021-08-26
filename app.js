bf3d52ec1906e57b3cf23495472108201c7555a8
brnhrdwnnr <bernhardwinner@gmail.com>
2021-08-26 20:51:52 +0700

    simple CSS Styling

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3006;
+const port = 2000;
 const router = require('./routes/index')
 const session = require('express-session')
 
